2fb3711878b1c1743e2303db3b0110200776accc
AJim98 <A.Jimaale98@outlook.com>
2023-11-09 19:04:32 +0000

    Milestone 3, functions added

--- milestone3.py
+++ milestone3.py
@@ -1,13 +1,23 @@
 word = "apple"
-while True:
-    guess = input("Enter an letter: ")
-    if guess.isalpha():
-        break
-    else:
-        print("Invalid input. Please enter a letter.")
-    
 
-if guess in word:
-    print(f"Nice guess! {guess} is in the word!")
-else:
-    print(f"Nice try, but {guess} isn't in the word. Try again")
+def check_guess():
+    guess = input("Enter an letter: ")
+    guess = guess.lower()
+    if guess in word:
+        print(f"Nice guess! {guess} is in the word!")
+    else:
+        print(f"Nice try, but {guess} isn't in the word. Try again")
+    return guess
+
+def ask_for_input():
+    guess = check_guess()
+    while True:
+        if guess.isalpha():
+            break
+        else:
+         print("Invalid input. Please enter a letter.")
+         break
+    return
+
+ask_for_input()
+
